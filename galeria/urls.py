4d4cce975ab7dda76b8a9f20ab0ea6535206a816
Leandro Ferreira D'Avila <leandro.davila@gmail.com>
2023-10-18 18:06:37 -0300

    Admin com customizações

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,5 +3,5 @@ from galeria.views import index, imagem
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem') #name é para usar nos templates {% url 'imagem' %}
+    path('imagem/<int:foto_id>', imagem, name='imagem') #name é para usar nos templates {% url 'imagem' %}
 ]
